ba27a7d3995be54b7258400b998f2d6064be4ae3
Kirill Semerenko <semerenkok@rambler.ru>
2022-03-04 14:20:08 +0300

    new

--- internal/httpReqests/httpRequests.go
+++ internal/httpReqests/httpRequests.go
@@ -17,12 +17,18 @@ const userStorageFile = "userStorage.json"
 
 func HttpMakeFriends(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	rawDataIn, err := ioutil.ReadFile(userStorageFile)
+	if err != nil {
+		_, _ = w.Write([]byte("No users in Storage"))
+	}
+	var userStorage user.Storage
+	_ = json.Unmarshal(rawDataIn, &userStorage)
 
 	var makeFriend makeFriends.MakeFriends
 	_ = json.NewDecoder(r.Body).Decode(&makeFriend)
 	var name1 string
 	var name2 string
-	_, err := strconv.Atoi(makeFriend.TargetId)
+	_, err = strconv.Atoi(makeFriend.TargetId)
 	if err != nil {
 		_, err := w.Write([]byte("ID must be int type"))
 		if err != nil {
@@ -38,7 +44,7 @@ func HttpMakeFriends(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	for _, u := range user.Users {
+	for _, u := range userStorage.Users {
 		if u.Id == makeFriend.TargetId {
 			name1 = u.Name
 		}
@@ -53,74 +59,14 @@ func HttpMakeFriends(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	for index, u := range user.Users {
+	for index, u := range userStorage.Users {
 		if u.Id == makeFriend.TargetId {
-			user.Users[index].Friends = append(user.Users[index].Friends, makeFriend.SourceId)
+			userStorage.Users[index].Friends = append(userStorage.Users[index].Friends, makeFriend.SourceId)
 		}
 		if u.Id == makeFriend.SourceId {
-			user.Users[index].Friends = append(user.Users[index].Friends, makeFriend.TargetId)
+			userStorage.Users[index].Friends = append(userStorage.Users[index].Friends, makeFriend.TargetId)
 		}
 	}
-
-	_, err = w.Write([]byte("User " + name1 + " and User " + name2 + " now friends! Status: " + strconv.Itoa(http.StatusOK)))
-	if err != nil {
-		return
-	}
-}
-func HttpGetUsers(w http.ResponseWriter, _ *http.Request) {
-	rawDataIn, err := ioutil.ReadFile(userStorageFile)
-	if err != nil {
-		_, _ = w.Write([]byte("No users in Storage"))
-	}
-	var userStorage user.Storage
-	_ = json.Unmarshal(rawDataIn, &userStorage)
-	_ = json.NewEncoder(w).Encode(userStorage)
-
-	var response string
-	for _, u := range user.Users {
-		response += u.ToString() + "\n"
-	}
-	_ = json.NewEncoder(w).Encode(user.Users)
-	_, err = w.Write([]byte(response))
-	if err != nil {
-		return
-	}
-
-}
-func HttpCreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var u user.User
-	_ = json.NewDecoder(r.Body).Decode(&u)
-
-	rawDataIn, _ := ioutil.ReadFile(userStorageFile)
-
-	var userStorage user.User
-	_ = json.NewDecoder(r.Body).Decode(rawDataIn)
-
-	_, err := strconv.Atoi(u.Age)
-	if err != nil {
-		_, err := w.Write([]byte("Age must be int type"))
-		if err != nil {
-			return
-		}
-		return
-	}
-	u.Id = strconv.Itoa(len(user.Users) + 1)
-	for i, u := range user.Users {
-		if u.Id != strconv.Itoa(i+1) {
-			u.Id = strconv.Itoa(i + 1)
-			break
-		}
-		if u.Id == u.Id {
-			id, _ := strconv.Atoi(u.Id)
-			u.Id = strconv.Itoa(id + 1)
-		}
-	}
-
-	//userStorage = append(userStorage, u)
-	//sort.SliceStable(userStorage.Users, func(i, j int) bool {
-	//	return userStorage.Users[i].Id < userStorage.Users[j].Id
-	//})
 	rawDataOut, err := json.MarshalIndent(&userStorage, "", "  ")
 	if err != nil {
 		log.Fatal("JSON marshaling failed:", err)
@@ -130,40 +76,122 @@ func HttpCreateUser(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal("Cannot write updated settings file:", err)
 	}
-	user.Users = append(user.Users, u)
-	sort.SliceStable(user.Users, func(i, j int) bool {
-		return user.Users[i].Id < user.Users[j].Id
-	})
 
-	w.WriteHeader(http.StatusCreated)
-	_, err = w.Write([]byte("User ID: " + u.Id + " Status:" + strconv.Itoa(http.StatusCreated)))
+	_, err = w.Write([]byte("User " + name1 + " and User " + name2 + " now friends! Status: " + strconv.Itoa(http.StatusOK)))
 	if err != nil {
 		return
 	}
 }
+
+func HttpGetUsers(w http.ResponseWriter, _ *http.Request) {
+	rawDataIn, err := ioutil.ReadFile(userStorageFile)
+	if err != nil {
+		_, _ = w.Write([]byte("No users in Storage"))
+	}
+	var userStorage user.Storage
+	_ = json.Unmarshal(rawDataIn, &userStorage)
+	_ = json.NewEncoder(w).Encode(userStorage)
+
+	//var response string
+	//for _, u := range user.Users {
+	//	response += u.ToString() + "\n"
+	//}
+	//_ = json.NewEncoder(w).Encode(user.Users)
+	//_, err = w.Write([]byte(response))
+	//if err != nil {
+	//	return
+	//}
+
+}
+
+func HttpCreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var us user.User
+	_ = json.NewDecoder(r.Body).Decode(&us)
+
+	rawDataIn, _ := ioutil.ReadFile(userStorageFile)
+
+	var userStorage user.Storage
+	_ = json.Unmarshal(rawDataIn, &userStorage)
+
+	_, err := strconv.Atoi(us.Age)
+	if err != nil {
+		_, err := w.Write([]byte("Age must be int type"))
+		if err != nil {
+			return
+		}
+		return
+	}
+	us.Id = strconv.Itoa(len(userStorage.Users) + 1)
+	for i, u := range userStorage.Users {
+		if u.Id != strconv.Itoa(i+1) {
+			us.Id = strconv.Itoa(i + 1)
+			break
+		}
+		if u.Id == u.Id {
+			id, _ := strconv.Atoi(u.Id)
+			u.Id = strconv.Itoa(id + 1)
+		}
+	}
+
+	userStorage.Users = append(userStorage.Users, us)
+	sort.SliceStable(userStorage.Users, func(i, j int) bool {
+		return userStorage.Users[i].Id < userStorage.Users[j].Id
+	})
+	rawDataOut, err := json.MarshalIndent(&userStorage, "", "  ")
+	if err != nil {
+		log.Fatal("JSON marshaling failed:", err)
+	}
+
+	err = ioutil.WriteFile(userStorageFile, rawDataOut, 0644)
+	if err != nil {
+		log.Fatal("Cannot write updated settings file:", err)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+
+	//_, err = w.Write([]byte("User ID: " + u.Id + " Status:" + strconv.Itoa(http.StatusCreated)))
+	//if err != nil {
+	//	return
+	//}
+	//id, _ := strconv.Atoi(u.Id)
+	_ = json.NewEncoder(w).Encode(us.Id)
+}
+
 func HttpGetUserFriends(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := chi.URLParam(r, "id")
-	for _, u := range user.Users {
+	rawDataIn, err := ioutil.ReadFile(userStorageFile)
+	if err != nil {
+		_, _ = w.Write([]byte("No users in Storage"))
+	}
+	var userStorage user.Storage
+	_ = json.Unmarshal(rawDataIn, &userStorage)
+	for _, u := range userStorage.Users {
 		if u.Id == params {
-			_, err := w.Write([]byte("User: " + u.Name + " Friends: " + u.FriendsToString()))
-			if err != nil {
-				return
-			}
+			id, _ := strconv.Atoi(u.Id)
+			_ = json.NewEncoder(w).Encode(&userStorage.Users[id-1].Friends)
 			return
 		}
 	}
-	_, err := w.Write([]byte("User not find"))
+	_, err = w.Write([]byte("User not find"))
 	if err != nil {
 		return
 	}
 }
+
 func HttpUpdateUserAge(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	rawDataIn, err := ioutil.ReadFile(userStorageFile)
+	if err != nil {
+		_, _ = w.Write([]byte("No users in Storage"))
+	}
+	var userStorage user.Storage
+	_ = json.Unmarshal(rawDataIn, &userStorage)
 	var updateAge updateUser.UpdateUser
 	_ = json.NewDecoder(r.Body).Decode(&updateAge)
 	params := chi.URLParam(r, "id")
-	_, err := strconv.Atoi(updateAge.NewAge)
+	_, err = strconv.Atoi(updateAge.NewAge)
 
 	if err != nil {
 		_, err := w.Write([]byte("Age must be int type"))
@@ -172,10 +200,18 @@ func HttpUpdateUserAge(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	for index, item := range user.Users {
+	for index, item := range userStorage.Users {
 		if item.Id == params {
-			user.Users[index].Age = updateAge.NewAge
-			_, err := w.Write([]byte("User " + item.Name + ". Age update successful! Status: " + strconv.Itoa(http.StatusOK)))
+			userStorage.Users[index].Age = updateAge.NewAge
+			rawDataOut, err := json.MarshalIndent(&userStorage, "", "  ")
+			if err != nil {
+				log.Fatal("JSON marshaling failed:", err)
+			}
+			err = ioutil.WriteFile(userStorageFile, rawDataOut, 0644)
+			if err != nil {
+				log.Fatal("Cannot write updated settings file:", err)
+			}
+			_, err = w.Write([]byte("User " + item.Name + ". Age update successful! Status: " + strconv.Itoa(http.StatusOK)))
 			if err != nil {
 				return
 			}
@@ -187,29 +223,45 @@ func HttpUpdateUserAge(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
 func HttpDeleteUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	rawDataIn, err := ioutil.ReadFile(userStorageFile)
+	if err != nil {
+		_, _ = w.Write([]byte("No users in Storage"))
+	}
+	var userStorage user.Storage
+	_ = json.Unmarshal(rawDataIn, &userStorage)
+
 	var makeFriend makeFriends.MakeFriends
 	_ = json.NewDecoder(r.Body).Decode(&makeFriend)
 
-	for i, u := range user.Users {
+	for i, u := range userStorage.Users {
 		for j, f := range u.Friends {
 			if f == makeFriend.TargetId {
-				user.Users[i].Friends = append(u.Friends[:j], u.Friends[j+1:]...)
+				userStorage.Users[i].Friends = append(u.Friends[:j], u.Friends[j+1:]...)
 			}
 		}
 	}
-	for index, u := range user.Users {
+	for index, u := range userStorage.Users {
 		if u.Id == makeFriend.TargetId {
-			user.Users = append(user.Users[:index], user.Users[index+1:]...)
-			_, err := w.Write([]byte(u.Name + " was delete. Status: " + strconv.Itoa(http.StatusOK)))
+			userStorage.Users = append(userStorage.Users[:index], userStorage.Users[index+1:]...)
+			rawDataOut, err := json.MarshalIndent(&userStorage, "", "  ")
+			if err != nil {
+				log.Fatal("JSON marshaling failed:", err)
+			}
+			err = ioutil.WriteFile(userStorageFile, rawDataOut, 0644)
+			if err != nil {
+				log.Fatal("Cannot write updated settings file:", err)
+			}
+			_, err = w.Write([]byte(u.Name + " was delete. Status: " + strconv.Itoa(http.StatusOK)))
 			if err != nil {
 				return
 			}
 			return
 		}
 	}
-	_, err := w.Write([]byte("User not found"))
+	_, err = w.Write([]byte("User not found"))
 	if err != nil {
 		return
 	}
--- internal/user/user.go
+++ internal/user/user.go
@@ -1,11 +1,6 @@
 package user
 
-import (
-	"fmt"
-	"strings"
-)
-
-var Users []User
+//var Users []User
 
 type User struct {
 	Id      string   `json:"id" bson:"_id,omitempty"`
@@ -19,22 +14,22 @@ type Storage struct {
 
 //var Database *mongo.Database
 
-func (u *User) FriendsToString() string {
-	var friend []string
-	var name string
-	for _, fr := range u.Friends {
-		for _, user := range Users {
-			if user.Id == fr {
-				name = user.Name
-			}
-		}
-		friend = append(friend, name)
-	}
-
-	friends := strings.Join(friend, ", ")
-	return friends
-}
-
-func (u *User) ToString() string {
-	return fmt.Sprintf("id %s name %s age %s friends %s", u.Id, u.Name, u.Age, u.FriendsToString())
-}
+//func (u *User) FriendsToString() string {
+//	var friend []string
+//	var name string
+//	for _, fr := range u.Friends {
+//		for _, user := range Users {
+//			if user.Id == fr {
+//				name = user.Name
+//			}
+//		}
+//		friend = append(friend, name)
+//	}
+//
+//	friends := strings.Join(friend, ", ")
+//	return friends
+//}
+//
+//func (u *User) ToString() string {
+//	return fmt.Sprintf("id %s name %s age %s friends %s", u.Id, u.Name, u.Age, u.FriendsToString())
+//}
